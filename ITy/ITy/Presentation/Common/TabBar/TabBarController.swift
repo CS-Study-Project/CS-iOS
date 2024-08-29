3f8e02666a9c031088fccf71c0f653bd0f634c4b
Cheon, Seongwoo <cjs1399@naver.com>
2024-08-29 13:27:42 +0900

    [Setting] #1 - Project File Folding

---
+++ ITy/ITy/Presentation/Common/TabBar/TabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarController.swift
+//  ITy
+//
+//  Created by 천성우 on 8/29/24.
+//
+
+import Foundation
---
+++ ITy/ITy/Presentation/Common/TabBar/TabBarItem.swift
@@ -0,0 +1,9 @@
+//
+//  TabBarItem.swift
+//  ITy
+//
+//  Created by 천성우 on 8/29/24.
+//
+
+import UIKit
+
