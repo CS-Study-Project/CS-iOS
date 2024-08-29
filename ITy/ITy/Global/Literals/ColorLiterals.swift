2b3127ec73fbe94f0a3a907dc1c96ce1c0f0d982
Cheon, Seongwoo <cjs1399@naver.com>
2024-08-29 14:01:56 +0900

    [Setting] #1 - 현재 개발 단계에서 불 필요한 코드 정정

--- ITy/ITy/Global/Literals/ColorLiterals.swift
+++ ITy/ITy/Global/Literals/ColorLiterals.swift
@@ -14,5 +14,61 @@ extension UIColor {
     static var gray50: UIColor {
         return UIColor(hex: "8B8B8D")
     }
+    
+    static var gray100: UIColor {
+        return UIColor(hex: "#8B8B8D")
+    }
+
+    static var gray200: UIColor {
+        return UIColor(hex: "#696969")
+    }
+    
+    static var gray250: UIColor {
+        return UIColor(hex: "#2F2E33")
+    }
+    
+    static var gray300: UIColor {
+        return UIColor(hex: "#6B6E82")
+    }
+    
+    static var gray400: UIColor {
+        return UIColor(hex: "#4A4D63")
+    }
+    
+    static var gray500: UIColor {
+        return UIColor(hex: "#2C2C37")
+    }
+    
+    static var gray600: UIColor {
+        return UIColor(hex: "#18171C")
+    }
+    
+    static var diamon000: UIColor {
+        return UIColor(hex: "#DEEFED")
+    }
+    
+    static var diamon700: UIColor {
+        return UIColor(hex: "#7FBBB3")
+    }
+    
+    static var white000: UIColor {
+        return UIColor(hex: "#FFFFFF")
+    }
+    
+    static var black000: UIColor {
+        return UIColor(hex: "#000000")
+    }
+
+    static var red300: UIColor {
+        return UIColor(hex: "#FF4539")
+    }
+    
+    static var red400: UIColor {
+        return UIColor(hex: "#FF4F17")
+    }
+    
+    static var red500: UIColor {
+        return UIColor(hex: "#E04F44")
+    }
 }
     
--- ITy/ITy/Global/Literals/ImageLiterals.swift
+++ ITy/ITy/Global/Literals/ImageLiterals.swift
@@ -9,7 +9,15 @@ import UIKit
 
 enum ImageLiterals {
     enum TabBar {
-        static var home_on: UIImage { .load(name: "home_ic_on").withRenderingMode(.alwaysOriginal) }
-        static var home_off: UIImage { .load(name: "home_ic_off").withRenderingMode(.alwaysOriginal) }
+        static var team_on: UIImage { .load(name: "ic_team_on").withRenderingMode(.alwaysOriginal) }
+        static var team_off: UIImage { .load(name: "ic_team_off").withRenderingMode(.alwaysOriginal) }
+        static var study_on: UIImage { .load(name: "ic_study_on").withRenderingMode(.alwaysOriginal) }
+        static var study_off: UIImage { .load(name: "ic_study_off").withRenderingMode(.alwaysOriginal) }
+        static var home_on: UIImage { .load(name: "ic_home_on").withRenderingMode(.alwaysOriginal) }
+        static var home_off: UIImage { .load(name: "ic_home_off").withRenderingMode(.alwaysOriginal) }
+        static var cs_on: UIImage { .load(name: "ic_cs_on").withRenderingMode(.alwaysOriginal) }
+        static var cs_off: UIImage { .load(name: "ic_cs_off").withRenderingMode(.alwaysOriginal) }
+        static var myPage_on: UIImage { .load(name: "ic_myPage_on").withRenderingMode(.alwaysOriginal) }
+        static var myPage_off: UIImage { .load(name: "ic_myPage_off").withRenderingMode(.alwaysOriginal) }
     }
 }
