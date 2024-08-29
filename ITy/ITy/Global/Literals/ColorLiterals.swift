202f22d08f8dd9d0f90ecbb27118565467054d6f
Cheon, Seongwoo <cjs1399@naver.com>
2024-08-29 13:42:26 +0900

    [Setting] #1 - add Literals

---
+++ ITy/ITy/Global/Literals/ColorLiterals.swift
@@ -0,0 +1,18 @@
+//
+//  ColorLiterals.swift
+//  ITy
+//
+//  Created by 천성우 on 8/29/24.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - Gray
+    
+    static var gray50: UIColor {
+        return UIColor(hex: "8B8B8D")
+    }
+}
+    
---
+++ ITy/ITy/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,15 @@
+//
+//  ImageLiterals.swift
+//  ITy
+//
+//  Created by 천성우 on 8/29/24.
+//
+
+import UIKit
+
+enum ImageLiterals {
+    enum TabBar {
+        static var home_on: UIImage { .load(name: "home_ic_on").withRenderingMode(.alwaysOriginal) }
+        static var home_off: UIImage { .load(name: "home_ic_off").withRenderingMode(.alwaysOriginal) }
+    }
+}
