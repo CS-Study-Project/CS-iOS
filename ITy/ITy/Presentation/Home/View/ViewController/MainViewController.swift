1abe1732433006b918a241ed3669ed58db6f29ee
Cheon, Seongwoo <cjs1399@naver.com>
2024-09-06 14:57:59 +0900

    [Fix] #3 - add File

--- ITy/ITy/Presentation/Home/View/ViewController/MainViewController.swift
+++ ITy/ITy/Presentation/Home/View/ViewController/MainViewController.swift
@@ -5,4 +5,34 @@
 //  Created by 천성우 on 9/3/24.
 //
 
-import Foundation
+import UIKit
+
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class MainViewController: BaseViewController {
+    
+    
+    private let viewModel = HomeViewModel()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Properties
+    
+    override func bindViewModel() {
+        
+    }
+    
+    override func setStyles() {
+        
+    }
+    
+    override func setLayout() {}
+    
+    
+}
+
