37872d1671842453c11c54160179dd2a5fe7a7d4
Cheon, Seongwoo <cjs1399@naver.com>
2024-09-12 10:59:47 +0900

    [Fix] #3 - Set Code

--- ITy/ITy/Presentation/Auth/View/ViewController/AuthViewController.swift
+++ ITy/ITy/Presentation/Auth/View/ViewController/AuthViewController.swift
@@ -5,4 +5,39 @@
 //  Created by 천성우 on 9/10/24.
 //
 
-import Foundation
+
+import UIKit
+
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class AuthViewController: BaseViewController {
+    
+    
+    private let viewModel = AuthViewModel()
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    private let profileView = ProfileView()
+    private let topicView = TopicView()
+    
+    
+    // MARK: - Properties
+    
+    override func bindViewModel() {
+        
+    }
+    
+    override func setStyles() {
+        view.backgroundColor = .gray10
+    }
+    
+    override func setLayout() {}
+    
+    
+}
+
+
--- ITy/ITy/Presentation/Auth/ViewModel/AuthViewModel.swift
+++ ITy/ITy/Presentation/Auth/ViewModel/AuthViewModel.swift
@@ -5,4 +5,29 @@
 //  Created by 천성우 on 9/10/24.
 //
 
-import Foundation
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+protocol AuthViewModelInput {
+}
+
+protocol AuthViewModelOutput {
+
+}
+
+protocol AuthViewModelType {
+    var inputs: AuthViewModelInput { get }
+    var outputs: AuthViewModelOutput { get }
+}
+
+final class AuthViewModel: AuthViewModelInput, AuthViewModelOutput, AuthViewModelType {
+    
+    var inputs: AuthViewModelInput { return self }
+    var outputs: AuthViewModelOutput { return self }
+    
+    init() {}
+}
+
+
