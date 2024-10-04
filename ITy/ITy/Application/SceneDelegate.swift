598b00858b81c537e2062a886ec8aad3fe008f08
Cheon, Seongwoo <cjs1399@naver.com>
2024-10-04 15:34:54 +0900

    [Fix] #3 - Connecting VC

--- ITy/ITy/Application/SceneDelegate.swift
+++ ITy/ITy/Application/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         
-        let mainViewController = VideoTrackViewController()
+        let mainViewController = AuthViewController()
 //        mainViewController.selectedIndex = 2
         guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
         sceneDelegate.window?.rootViewController = UINavigationController(rootViewController: mainViewController)
