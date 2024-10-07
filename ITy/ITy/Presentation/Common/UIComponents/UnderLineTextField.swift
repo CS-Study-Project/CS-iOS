7cf1b0773f1d5296a5a905d99d97229c0c6ee88f
Cheon, Seongwoo <cjs1399@naver.com>
2024-10-07 16:06:20 +0900

    [Feature] #3 - Custom UnderLineTextField

--- ITy/ITy/Presentation/Common/UIComponents/UnderLineTextField.swift
+++ ITy/ITy/Presentation/Common/UIComponents/UnderLineTextField.swift
@@ -11,69 +11,84 @@ import UIKit
 import Then
 import SnapKit
 
-class UnderLineTextField: UITextField {
+class UnderlineTextField: UITextField {
     
-    lazy var placeholderColor: UIColor = .white000
-    lazy var placeholderString: String = ""
+    // 언더라인 레이어
+    private let underlineLayer = CALayer()
     
-    private lazy var underLineView: UIView = {
-        let lineView = UIView()
-        lineView.backgroundColor = .white000
-        return lineView
-    } ()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        addSubview(underLineView)
-        underLineView.snp.makeConstraints {
-            $0.top.equalTo(self.snp.bottom)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(1)
+    // 기본 상태에서의 언더라인 색상
+    var defaultUnderlineColor: UIColor = .gray30 {
+        didSet {
+            updateUnderlineColor()
         }
-        
-        self.addTarget(self, action: #selector(editingDidBegin), for: .editingDidBegin)
-        self.addTarget(self, action: #selector(editingDidEnd), for: .editingDidEnd)
+    }
+    
+    // 활성화 상태에서의 언더라인 색상
+    var activeUnderlineColor: UIColor = .black000
+    
+    // 오류 상태에서의 언더라인 색상
+    var errorUnderlineColor: UIColor = .red300
+    
+    // 오류 상태인지 여부
+    var isError: Bool = false {
+        didSet {
+            updateUnderlineColor()
+        }
+    }
 
+    init(frame: CGRect, placeholderString: String) {
+        super.init(frame: frame)
+        placeholder = placeholderString
+        setupUnderline()
+        setupObservers()
     }
     
-    func setPlaceholder(placeeholder: String, color: UIColor) {
-        placeholderString = placeeholder
-        placeholderColor = color
-        
-        setPlaceholder()
-        underLineView.backgroundColor = placeholderColor
+    // 언더라인 설정
+    private func setupUnderline() {
+        borderStyle = .none
+        underlineLayer.backgroundColor = defaultUnderlineColor.cgColor
+        layer.addSublayer(underlineLayer)
     }
     
-    func setPlaceholder() {
-        self.attributedPlaceholder = NSAttributedString(
-            string: placeholderString,
-            attributes: [NSAttributedString.Key.foregroundColor: placeholderColor]
-        )
+    // 텍스트 필드의 프레임에 맞춰 언더라인 위치 및 크기 설정
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let underlineHeight: CGFloat = 1.0
+        underlineLayer.frame = CGRect(x: 0, y: frame.height - underlineHeight, width: frame.width, height: underlineHeight)
     }
     
-    func setError() {
-        self.attributedPlaceholder = NSAttributedString(
-            string: placeholderString,
-            attributes: [NSAttributedString.Key.foregroundColor: UIColor.red300]
-        )
-        underLineView.backgroundColor = .red300
+    // 언더라인 색상을 업데이트하는 메서드
+    private func updateUnderlineColor() {
+        if isError {
+            underlineLayer.backgroundColor = errorUnderlineColor.cgColor
+        } else if isFirstResponder {
+            underlineLayer.backgroundColor = activeUnderlineColor.cgColor
+        } else {
+            underlineLayer.backgroundColor = defaultUnderlineColor.cgColor
+        }
     }
+    
+    // 상태 변화에 따라 언더라인 색상을 업데이트
+    private func setupObservers() {
+        addTarget(self, action: #selector(editingBegan), for: .editingDidBegin)
+        addTarget(self, action: #selector(editingEnded), for: .editingDidEnd)
+    }
+    
+    @objc private func editingBegan() {
+        updateUnderlineColor()
+    }
+    
+    @objc private func editingEnded() {
+        updateUnderlineColor()
+    }
+    
+    // 비밀번호 입력 타입 설정
+    func setPasswordMode() {
+        isSecureTextEntry = true
+    }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension UnderLineTextField {
-    @objc
-    func editingDidBegin() {
-        setPlaceholder()
-        underLineView.backgroundColor = .white000
-    }
-    
-    @objc
-    func editingDidEnd() {
-        underLineView.backgroundColor = placeholderColor
     }
 }
