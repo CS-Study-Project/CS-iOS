0ec5f3ca10da3b9ac9a0db700bfa992983263766
Cheon, Seongwoo <cjs1399@naver.com>
2024-10-07 16:06:34 +0900

    [Feature] #3 - add AuthView UI

--- ITy/ITy/Presentation/Auth/View/AuthView.swift
+++ ITy/ITy/Presentation/Auth/View/AuthView.swift
@@ -15,18 +15,82 @@ class AuthView: BaseView {
     // MARK: - UI Components Property
     
     
+    private let authViewTitle = UILabel()
+    private let leftLine = UIView()
+    private let rightLine = UIView()
+    private let kakaoLoginButton = UIButton()
+    private let appleLoginButton = UIButton()
+    
     
     override func setStyles() {
         backgroundColor = .white000
         layer.cornerRadius = 16
+        
+        authViewTitle.do {
+            $0.text = "간편 로그인"
+            $0.textColor = .gray30
+            $0.font = .fontGuide(.detail2_reg)
+        }
+        
+        leftLine.do {
+            $0.backgroundColor = .gray30
+        }
+        
+        rightLine.do {
+            $0.backgroundColor = .gray30
+        }
+        
+        kakaoLoginButton.do {
+            $0.setImage(ImageLiterals.auth.kakaoLogin_btn, for: .normal)
+            $0.layer.cornerRadius = 5
+        }
+        
+        appleLoginButton.do {
+            $0.setImage(ImageLiterals.auth.appleLogin_btn, for: .normal)
+            $0.layer.cornerRadius = 5
+        }
 
     }
 
     // MARK: - Layout Helper
 
     override func setLayout() {
-//        addSubviews()
+        addSubviews(authViewTitle, leftLine, rightLine, kakaoLoginButton, appleLoginButton)
+
         
+        
+        leftLine.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(SizeLiterals.Screen.screenWidth * 20 / 375)
+            $0.centerY.equalTo(authViewTitle.snp.centerY)
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth * 130 / 375)
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 0.5 / 812)
+        }
+        
+        authViewTitle.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(SizeLiterals.Screen.screenHeight * 13 / 375)
+            $0.centerX.equalToSuperview()
+        }
+        
+        rightLine.snp.makeConstraints {
+            $0.leading.equalTo(authViewTitle.snp.trailing).offset(SizeLiterals.Screen.screenWidth * 8 / 375)
+            $0.centerY.equalTo(authViewTitle.snp.centerY)
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth * 130 / 375)
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 0.5 / 812)
+        }
+        
+        appleLoginButton.snp.makeConstraints {
+            $0.top.equalTo(authViewTitle.snp.bottom).offset(SizeLiterals.Screen.screenHeight * 42 / 812)
+            $0.leading.equalToSuperview().offset(SizeLiterals.Screen.screenWidth * 15.75 / 375)
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth * 343 / 375)
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 50 / 812)
+        }
+        
+        kakaoLoginButton.snp.makeConstraints {
+            $0.top.equalTo(appleLoginButton.snp.bottom).offset(SizeLiterals.Screen.screenHeight * 17 / 812)
+            $0.leading.equalToSuperview().offset(SizeLiterals.Screen.screenWidth * 15.75 / 375)
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth * 343 / 375)
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 50 / 812)
+        }
     }
     
     // MARK: - Methods
